8372986d277353c6163c6fd596dab74a5d24ab1b
hoa <huynhtrunghoa186@gmail.com>
2023-09-20 20:33:19 +0700

    first commit

---
+++ routes/product/router.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+const { validateSchema, checkIdSchema } = require('../../utils')
+
+const { getDetail, getList, getAll, search, create, update, updatePatch, hardDelete, softDelete, fake } = require('./controller');
+const { validationSchema, validationQuerySchema } = require('./validation');
+
+router.route('/all')
+  .get(getAll);
+
+router.route('/')
+  .get(getList)
+  .post(validateSchema(validationSchema), create)
+
+router.route('/fake')
+  .post(fake)
+
+router.route('/')
+  .get(getList)
+  .post(validateSchema(validationSchema), create)
+
+router.get('/search', validateSchema(validationQuerySchema), search);
+
+router.route('/:id')
+  .get(validateSchema(checkIdSchema), getDetail)
+  .put(validateSchema(checkIdSchema), validateSchema(validationSchema), update)
+
+router.patch('/delete/:id', softDelete);
+
+module.exports = router;
